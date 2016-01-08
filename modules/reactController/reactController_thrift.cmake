66e6f663a866ae24121e2b0c7fdb5d4b53e6eac5
Ugo Pattacini <ugo.pattacini@iit.it>
2016-01-08 16:41:44 +0100

    used yarp_add_idl macro

--- modules/reactController/reactController_thrift.cmake
+++
@@ -1,5 +0,0 @@
-## This is an automatically-generated file.
-## It could get re-generated if the ALLOW_IDL_GENERATION flag is on
-
-set(headers include/reactController_IDL.h)
-set(sources src/reactController_IDL.cpp)
