7d4b8a0887f077a541d69231c2f9013acdd3da8d
Matej Hoffmann <matej.hoffmann@iit.it>
2016-02-15 18:59:20 +0100

    finished script to plot joint vel identification

--- matlab/jointVelCtrlIdent/analyzeJointVelCtrlIdent.m
+++ matlab/jointVelCtrlIdent/analyzeJointVelCtrlIdent.m
@@ -1,22 +1,34 @@
 % Author: Matej Hoffmann
 clear; 
 
-save_figs = false;
+visualize_time_stats = true;
+save_figs = true;
 chosen_time_column = 6; % 6 for receiver, 4 for sender
 
-path_prefix_sim = 'icubSimTests/';
+path_prefix_robot = 'icubTests/'; % 'icubSimTests/' or 'icubTests';
 path_prefix_arm = 'arm_joint_';
+path_prefix_torso = 'torso_joint_';
 
-joints(1).path_prefix = [path_prefix_sim path_prefix_arm '0/'];
-joints(1).name = '1st shoulder';
-joints(2).path_prefix = [path_prefix_sim path_prefix_arm '1/'];
-joints(2).name = '2nd shoulder' 
+joints(1).path_prefix = [path_prefix_robot path_prefix_torso '0/'];
+joints(1).name = 'torso - yaw';
+joints(2).path_prefix = [path_prefix_robot path_prefix_torso '1/'];
+joints(2).name = 'torso - roll';
+joints(3).path_prefix = [path_prefix_robot path_prefix_torso '2/'];
+joints(3).name = 'torso - pitch';
+
+joints(4).path_prefix = [path_prefix_robot path_prefix_arm '0/'];
+joints(4).name = '1st shoulder';
+joints(5).path_prefix = [path_prefix_robot path_prefix_arm '1/'];
+joints(5).name = '2nd shoulder'; 
+joints(6).path_prefix = [path_prefix_robot path_prefix_arm '2/'];
+joints(6).name = '3rd shoulder'; 
+joints(7).path_prefix = [path_prefix_robot path_prefix_arm '3/'];
+joints(7).name = '1st elbow'; 
+joints(8).path_prefix = [path_prefix_robot path_prefix_arm '4/'];
+joints(8).name = '2nd elbow'; 
+
 
 path_suffix = 'data/jointVelCtrlIdent/data.log';
-
-if save_figs
-  mkdir('output');
-end
 
 NR_EXTRA_TIME_COLUMNS = 4; % these will be created so that there is time starting from 0, plus time increment column - this 2 times (sender and receiver time stamp) - for diagnostics
 
@@ -24,84 +36,105 @@ NR_EXTRA_TIME_COLUMNS = 4; % these will be created so that there is time startin
 %1: packetID, 2: sender time stamp, 3:receiver time stamp, 
 %4: time from 0, sender; 5: increments of 4; 6: time from 0 receiver; 7: increments in 6 
 % 8: counter, 9: velocity command, 10: reference 
-%11-12: pwm; 13: joint pos feedback; 14: vel estimated from derivative of
+%11 or 11-12: pwm; 12/13: joint pos feedback; 13/14: vel estimated from derivative of
 %position feedback
 
-for i=1:size(joints)
-    joints(i).d_orig=importdata([joints(i).path_prefix path_suffix]);
-end
-
+PACKET_ID_COLUMN = 1;
+TIME_ABS_1_COLUMN = 2;
+TIME_ABS_2_COLUMN = 3;
 TIME_FROM_ZERO_1_COLUMN = 4;
 TIME_FROM_ZERO_DELTA_1_COLUMN = 5;
 TIME_FROM_ZERO_2_COLUMN = 6;
 TIME_FROM_ZERO_DELTA_2_COLUMN = 7;
-PACKET_ID_COLUMN = 1;
-TIME_ABS_1_COLUMN = 2;
-TIME_ABS_2_COLUMN = 3;
-
 vel_command_col = 9;
-pos_fb_col = 13;
-vel_estimated_col = 14;
-
+        
+for i=1:length(joints)
+    joints(i).d_orig=importdata([joints(i).path_prefix path_suffix]);
+    if ( size(joints(i).d_orig,2) == 9) % joints with 1 pwm col 
+        joints(i).pos_fb_col = 12;
+        joints(i).vel_estimated_col = 13;
+    elseif (size(joints(i).d_orig,2) == 10)  % joints with 2 pwm cols
+        joints(i).pos_fb_col = 13;
+        joints(i).vel_estimated_col = 14;
+    else
+        error('Unexptected nr cols');
+    end
+end
 
 
 %% create extra time columns
 
-    nr_rows_dat = size(d_orig,1);
+for i=1:length(joints)
+    nr_rows_dat = size(joints(i).d_orig,1);
     time_cols_to_insert = NaN(nr_rows_dat,4);
     time_cols_to_insert(1,1)=0; time_cols_to_insert(1,2)=0;time_cols_to_insert(1,3)=0; time_cols_to_insert(1,4)=0;
-    for i=2:nr_rows_dat
-        time_cols_to_insert(i,1)=d_orig(i,2)-d_orig(1,2); % sender time stamp
-        time_cols_to_insert(i,2)=time_cols_to_insert(i,1)-time_cols_to_insert(i-1,1); % increment - to see if it is stable
-        time_cols_to_insert(i,3)=d_orig(i,3)-d_orig(1,3); % this would be the receiver time stamp
-        time_cols_to_insert(i,4)=time_cols_to_insert(i,3)-time_cols_to_insert(i-1,3); % increment - to see if it is stable
+    for j=2:nr_rows_dat
+        time_cols_to_insert(j,1)=joints(i).d_orig(j,2)-joints(i).d_orig(1,2); % sender time stamp
+        time_cols_to_insert(j,2)=time_cols_to_insert(j,1)-time_cols_to_insert(j-1,1); % increment - to see if it is stable
+        time_cols_to_insert(j,3)=joints(i).d_orig(j,3)-joints(i).d_orig(1,3); % this would be the receiver time stamp
+        time_cols_to_insert(j,4)=time_cols_to_insert(j,3)-time_cols_to_insert(j-1,3); % increment - to see if it is stable
     end
-    d = [d_orig(:,1:3) time_cols_to_insert(:,1:4) d_orig(:,4:end)];
+    joints(i).d = [joints(i).d_orig(:,1:3) time_cols_to_insert(:,1:4) joints(i).d_orig(:,4:end)];
     
-
+end
+    
 %% info about data matrix
 
-sz=size(d);
-L=sz(1); % ~ nr. rows
-
-t = d(:,chosen_time_column); 
-
+for i=1:length(joints)
+    sz=size(joints(i).d);
+    joints(i).L=sz(1); % ~ nr. rows
+    joints(i).t = joints(i).d(:,chosen_time_column); 
+end
 
 %% plot time diagnostics
 
 if visualize_time_stats
-    f13 = figure(13); clf;
-    set(f13,'Name','Time statistics');
-
-    subplot(3,1,1);
-        title('Absolute time');
-        hold on;
-          plot(d(:,TIME_ABS_1_COLUMN),'b+');
-          plot(d(:,TIME_ABS_2_COLUMN),'k+');
-        hold off;
-        legend('time (sender)','time (receiver)');
-        ylabel('Time (s)');
-       
-    subplot(3,1,2)
-        title('Time starting from 0');
-        hold on;
-          plot(d(:,TIME_FROM_ZERO_1_COLUMN),'b+');
-          plot(d(:,TIME_FROM_ZERO_2_COLUMN),'k+');
-        hold off;
-        legend('time (sender)','time (receiver)');
-        ylabel('Time (s)');
-       
-     subplot(3,1,3);
-           title(' Time increments');
-           hold on;
-             plot(d(2:end,TIME_FROM_ZERO_DELTA_1_COLUMN),'b+');
-             plot(d(2:end,TIME_FROM_ZERO_DELTA_2_COLUMN),'k+');
-           hold off;
-           legend('joints (sender)','joints (receiver)');
-           ylabel('Sampling - delta time (s)');
-       
-    if save_figs
-       saveas(f13,'TimeStats.fig'); 
-    end
+    f1 = figure(1); clf;
+    set(f1,'Name','Time increments');
+  
+   for i=1:length(joints)    
+     subplot(3,3,i);
+       hold on;
+        title(joints(i).name);
+        plot(joints(i).d(2:end,TIME_FROM_ZERO_DELTA_1_COLUMN),'b+');
+        plot(joints(i).d(2:end,TIME_FROM_ZERO_DELTA_2_COLUMN),'k+');
+        legend('time stamp (sender)','time stamp (receiver)');
+      hold off;
+   end       
+           
+   if save_figs
+     saveas(f1,'TimeStats.fig'); 
+   end
 
 end
+
+%% plot frequency response
+
+
+f2 = figure(2); clf;
+set(f2,'Name','Frequency response');
+   
+   
+    for i=1:length(joints)    
+     subplot(3,3,i);
+       hold on;
+        title(joints(i).name);
+        [AX,H1,H2] = plotyy(joints(i).t,joints(i).d(:,joints(i).pos_fb_col),joints(i).t,joints(i).d(:,vel_command_col));   
+         hold(AX(1),'on');hold(AX(2),'on');
+         H3 = plot(AX(2),joints(i).t,joints(i).d(:,joints(i).vel_estimated_col), 'Parent', AX(2), 'LineStyle',':','Color','c','Marker','v','MarkerSize',2);
+         hold(AX(1),'off');hold(AX(2),'off');
+         set(get(AX(1),'Ylabel'),'String','joint pos [deg]'); 
+         %set(AX(1),'Ylim',[joint_info(j).pos_limit_min-5 joint_info(j).pos_limit_max+5]); 
+         set(get(AX(2),'Ylabel'),'String','Joint velocity [deg/s]');
+         %set(AX(2),'Ylim',[joint_info(j).vel_limit_min-3 joint_info(j).vel_limit_max+3]); 
+         xlabel('t [s]');
+         legend('pos','vel','vel estimate');
+         %set(H1,'LineStyle','-');
+         %set(H2,'LineStyle','--');
+      hold off;
+    end 
+   
+   if save_figs
+     saveas(f2,'FrequencyResponse.fig'); 
+   end
+
